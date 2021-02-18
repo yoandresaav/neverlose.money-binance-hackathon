56b7b367ea57fb259813812208d95c88674a8254
ggomaeng <sungwoopark95@gmail.com>
2021-02-18 14:34:35 -0800

    Only allow BSC Testnet

--- web/src/utils/chainId.js
+++ web/src/utils/chainId.js
@@ -8,7 +8,7 @@ export function allowedChainId(id) {
   // return process.env.NODE_ENV === "production"
   //   ? [CHAINID_MAINNET].includes(id)
   //   : [CHAINID_MAINNET].includes(id);
-  return [CHAINID_MAINNET, CHAIN_ID_BSC_MAIN, CHAIN_ID_BSC_TESTNET].includes(
+  return [CHAIN_ID_BSC_TESTNET].includes(
     id
   );
 }
