753e2e7c11d57092ccc7ab53ff24b5a64242b907
ggomaeng <sungwoopark95@gmail.com>
2021-02-18 19:25:48 -0800

    Remove a RPC node

--- web/src/utils/Contract.js
+++ web/src/utils/Contract.js
@@ -9,7 +9,7 @@ import random from "lodash/random";
 
 // Array of available nodes to connect to
 const nodes = [
-  "https://data-seed-prebsc-2-s1.binance.org:8545",
+  // "https://data-seed-prebsc-2-s1.binance.org:8545",
   "https://data-seed-prebsc-1-s2.binance.org:8545",
   "https://data-seed-prebsc-2-s2.binance.org:8545",
   "https://data-seed-prebsc-1-s3.binance.org:8545",
