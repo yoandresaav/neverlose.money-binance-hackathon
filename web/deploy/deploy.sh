d62e1f2fc76d485527140f0da105111d12ec963d
Sebastian Kim <sydneyitguy@gmail.com>
2021-02-19 10:07:46 +0900

    Update favicons

--- web/deploy/deploy.sh
+++ web/deploy/deploy.sh
@@ -1,5 +1,5 @@
-git push && git pull && yarn && GENERATE_SOURCEMAP=true yarn build &&
-node deploy/upload-source-maps.js  && # will delete all map files before upload
+git push && git pull && yarn && GENERATE_SOURCEMAP=true yarn build
+# node deploy/upload-source-maps.js  && # will delete all map files before upload
 
 TIMESTAMP=`date +%Y-%m-%d-%H-%M-%S` &&
 ssh neverlose "mkdir -p /srv/web/bsc-test.neverlose/releases/$TIMESTAMP"  &&
